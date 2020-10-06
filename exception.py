424d9ce164dc721d318e4e602f2d5064d02054af
Mousamdey98 <57684227+Mousamdey98@users.noreply.github.com>
2020-10-06 09:55:18 +0530

    adding working function finally and else

--- exception.py
+++ exception.py
@@ -5,3 +5,8 @@ try:
 except NameError as e:
         print ("An exception occurred")
         print (e)
+else:
+    print('Everything is fine in try-except')
+    
+finally:
+    print('out of try except')
